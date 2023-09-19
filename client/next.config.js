22dda6a03c50fa1ad8d23f236ee1cc7e85a42a92
sunshine2329 <seniordeveloper00001@gmail.com>
2023-09-19 03:45:28 -0400

    feat: setup server and client

---
+++ client/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+const nextConfig = withBundleAnalyzer({
+  reactStrictMode: true
+})
+
+module.exports = nextConfig
